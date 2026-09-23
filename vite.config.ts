22a637db0936322c5a91e3c100781ab40e4ca441
Malik Schenk <malik.schenk1@gmail.com>
2026-09-23 20:13:32 +0200

    chore: update base path in vite config
    Adjust base path to /Piggi/ to ensure correct asset resolution for production deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './Piggi',
+    base: '/Piggi/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
